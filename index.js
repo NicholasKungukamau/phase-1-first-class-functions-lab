e8d1ad296ee9bb3bd32e2e87bbe6ec1828e30129
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-25 09:56:40 +0300

    commit tree 5af4169a54f0d565481d573f3e2af30b7ac98286

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+ function returnFirstTwoDrivers(drivers) { 
+     return drivers.slice(0,2);
+ }
+
+ function returnLastTwoDrivers(drivers) {
+     return drivers.slice(-2, drivers.length);   
+ }
+ 
+ const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+ function  createFareMultiplier(n) {
+     return function () {
+         return n * n;
+     }
+ }
+
+ function fareDoubler(n) {
+     return n * 2;
+ }
+
+ function fareTripler(f) {
+     return f * 3;
+ }
+
+ function selectDifferentDrivers(arrayOfDrivers, method) {
+     if (selectingDrivers[0] ===method) {
+         return method(arrayOfDrivers);
+     } else {
+         return method(arrayOfDrivers);
+     }
+     }
+ 
